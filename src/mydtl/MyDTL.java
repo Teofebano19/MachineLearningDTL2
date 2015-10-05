64f82d6ca7ff2cce09f1f47abc3cbce66a1c860c
Teofebano <teofebano.stei@gmail.com>
2015-10-05 12:06:59 +0700

    first commit

---
+++ src/mydtl/MyDTL.java
@@ -0,0 +1,164 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mydtl;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import weka.classifiers.Classifier;
+import weka.classifiers.Evaluation;
+import weka.classifiers.trees.Id3;
+import weka.classifiers.trees.J48;
+import weka.core.Debug;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils;
+import weka.filters.Filter;
+import weka.filters.supervised.instance.Resample;
+import weka.filters.unsupervised.attribute.Remove;
+
+/**
+ *
+ * @author Teofebano
+ */
+public class MyDTL {
+    // Attribute
+    private static final String SOURCE = "data/weather.nominal.arff";
+    private static final String SOURCE_UNLABELED = "data/weather.nominal.unlabeled.arff";
+    private static final int NUMBER_FOLD = 10;
+    private static final int PERCENTAGE = 66;
+    public static Instances data;
+    
+    // Load file
+    public static void loadFile(String source){
+        try {
+            data = ConverterUtils.DataSource.read(source);
+            if (data.classIndex() == -1){
+                data.setClassIndex(data.numAttributes()-1);
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(MyDTL.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    // 10-fold
+    public static void learn10fold(Instances trainingData, Classifier classifier){
+        try {
+            // Build and Eval
+            Evaluation eval = new Evaluation(trainingData);
+            eval.crossValidateModel(classifier, trainingData, NUMBER_FOLD, new Debug.Random(1));
+            
+            // Print
+            System.out.println("=== Summary ===");
+            System.out.println(eval.toSummaryString());
+        } catch (Exception ex) {
+            Logger.getLogger(MyDTL.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    // percentage split
+    public static void learnFull(Instances trainingData, Classifier classifier){
+        try {
+            // Build
+            Classifier cls = classifier;
+            int trainSize = (int) Math.round(trainingData.numInstances() * PERCENTAGE/ 100);
+            int testSize = trainingData.numInstances() - trainSize;
+            Instances train = new Instances(trainingData, 0, trainSize);
+            Instances test = new Instances(trainingData, trainSize, testSize);
+            cls.buildClassifier(train);
+            // Eval
+            Evaluation eval = new Evaluation(trainingData);
+            eval.evaluateModel(classifier, test);
+            
+            // Print
+            System.out.println("=== Summary ===");
+            System.out.println(eval.toSummaryString());
+        } catch (Exception ex) {
+            Logger.getLogger(MyDTL.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    // save model
+    public static void saveModel(Instances trainingData, Classifier classifier, String file){
+        try {
+            Classifier cls = classifier;
+            cls.buildClassifier(trainingData);
+            try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file))) {
+                oos.writeObject(cls);
+                oos.flush();
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(MyDTL.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    // load model
+    public static void loadModel(String file){
+        ObjectInputStream ois;
+        try {
+            Classifier cls = null;
+            ois = new ObjectInputStream(new FileInputStream(file));
+            cls = (Classifier) ois.readObject(); 
+            ois.close();
+        } catch (FileNotFoundException e){
+        } catch (IOException ex) {
+            Logger.getLogger(MyDTL.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(MyDTL.class.getName()).log(Level.SEVERE, null, ex);
+        } 
+    }
+    
+    // classification
+    public static void classifyUsingModel(Classifier classifier, String file){
+        try {
+            Instances unlabeled = ConverterUtils.DataSource.read(file);
+            unlabeled.setClassIndex(unlabeled.numAttributes() - 1);
+            Instances labeled = new Instances(unlabeled);
+            // label instances
+            for (int i = 0; i < unlabeled.numInstances(); i++) {
+                double clsLabel = classifier.classifyInstance(unlabeled.instance(i));
+                labeled.instance(i).setClassValue(clsLabel);
+                System.out.println(labeled.lastInstance().toString());
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(MyDTL.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    // attribute removal
+    public static Instances removeAttribute(String file, String indices, Boolean invert ) throws Exception{
+        Instances newdata;
+        Remove remove = new Remove();
+        remove.setAttributeIndices(indices);
+        remove.setInvertSelection(invert.booleanValue());
+        remove.setInputFormat(data);
+        newdata = Filter.useFilter(data, remove);
+        return newdata;
+    }
+    
+    // filter resample
+    public static Instances resample(Instances data) throws Exception{
+        Instances newdata;
+        Resample resample = new Resample();
+        resample.setInputFormat(data);
+        newdata = Filter.useFilter(data, resample);
+        return newdata;
+    }
+    
+    // main
+    public static void main(String[] args) {
+        Classifier DT = new J48();
+        Classifier ID3 = new Id3();
+        myID3 id3 = new myID3();
+        myJ48 j48 = new myJ48();
+        loadFile(SOURCE);
+        learnFull(data, j48);
+    }    
+}
---
+++ src/mydtl/myID3.java
@@ -0,0 +1,183 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mydtl;
+
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.Vector;
+import weka.classifiers.Classifier;
+import weka.core.Attribute;
+import weka.core.Capabilities;
+import weka.core.Capabilities.Capability;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.NoSupportForMissingValuesException;
+import weka.core.Utils;
+
+/**
+ *
+ * @author Teofebano
+ */
+public class myID3 extends Classifier{
+    // Attribute
+    myID3[] child;
+    private Attribute attrSeparator;
+    private double[] result;
+    private double classValue;
+    
+    @Override
+    public void buildClassifier(Instances data) throws Exception {
+        // can classifier handle the data?
+        getCapabilities().testWithFail(data);
+
+        // remove instances with missing class
+        data = new Instances(data);
+        data.deleteWithMissingClass();
+        buildTree(data);
+    }
+    
+    private double computeEntropy(Instances data){
+        double entropy = 0;
+        // Class
+        int numClasses = data.numClasses();
+        Vector<Double> classCounter = new Vector<Double>();
+        classCounter.setSize(numClasses);
+        for (int i=0;i<numClasses;i++){
+            classCounter.setElementAt(Double.valueOf(0), i);
+        }
+
+        int numInstances = data.numInstances();
+        for (int i=0;i<numInstances;i++){
+            int cv = (int) data.instance(i).classValue();
+            classCounter.setElementAt(classCounter.elementAt(cv)+1, cv);
+        }
+        // Entropy calculation for each class
+        for (int i=0;i<data.numClasses();i++){
+            if (classCounter.elementAt(i)>0){
+                double probability = classCounter.elementAt(i) / numInstances;
+                entropy -= (probability * Utils.log2(probability));
+            }
+        }
+        return entropy;
+    }
+    
+    private double computeIG(Instances data, Attribute attr){
+       double IG = computeEntropy(data);
+       Instances[] instances = split(data,attr);
+       for (int i=0;i<instances.length;i++){
+           if (instances[i].numInstances() > 0){
+               IG -= (instances[i].numInstances() / data.numInstances()) * computeEntropy(instances[i]);
+           }
+       }
+       return IG;
+    }
+    
+    private Instances[] split(Instances data, Attribute attr){
+        Instances[] splitData = new Instances[attr.numValues()];
+        for (int j = 0; j < attr.numValues(); j++) {
+          splitData[j] = new Instances(data, data.numInstances());
+        }
+        Enumeration instEnum = data.enumerateInstances();
+        while (instEnum.hasMoreElements()) {
+          Instance inst = (Instance) instEnum.nextElement();
+          splitData[(int) inst.value(attr)].add(inst);
+        }
+        for (int i = 0; i < splitData.length; i++) {
+          splitData[i].compactify();
+        }
+        return splitData;
+    }
+    
+    private void buildTree(Instances trainingData) {
+        // zero instance
+        if (trainingData.numInstances() == 0){
+            attrSeparator = null;
+            result = new double[trainingData.numClasses()];
+            classValue = Instance.missingValue();
+            return;
+        }
+        
+        // search for highest IG
+        Vector<Double> listIG =  new Vector<Double>();
+        int numAttribute = trainingData.numAttributes();
+        listIG.setSize(numAttribute);
+        for (int i=0;i<numAttribute;i++){
+            listIG.setElementAt(Double.valueOf(0), i);
+        }
+        for (int i=0;i<numAttribute;i++){
+            Attribute attr = trainingData.attribute(i);
+            int attrIndex = attr.index();
+            listIG.setElementAt(computeIG(trainingData, attr), attrIndex);
+        }
+        int indexMaxIG = listIG.indexOf(Collections.max(listIG));
+        attrSeparator = trainingData.attribute(indexMaxIG);
+        
+        // set the root for the tree
+        if (listIG.elementAt(indexMaxIG) == 0){ // leaf
+            attrSeparator = null;
+            int numClasses = trainingData.numClasses();
+            int numInstances = trainingData.numInstances();
+            result = new double[numClasses];
+            for (int i=0;i<numInstances;i++){
+                result[(int)trainingData.instance(i).classValue()]++;
+            }
+            Utils.normalize(result);
+            classValue = Utils.maxIndex(result);
+        }
+        else{ // branch
+            Instances[] splittedData = split(trainingData,attrSeparator);
+            int size = attrSeparator.numValues();
+            child = new myID3[size];
+            for (int i=0;i<size;i++){
+                child[i] = new myID3();
+                child[i].buildTree(splittedData[i]);
+            }
+        }
+    }
+    
+    @Override
+    public double classifyInstance(Instance testingData) throws NoSupportForMissingValuesException, Exception{
+        if (testingData.hasMissingValue()) {
+            throw new NoSupportForMissingValuesException("MyID3 can't handle such missing value");
+        }
+        if (attrSeparator == null){
+            return classValue;
+        }
+        else{
+            return child[(int) testingData.value(attrSeparator)].classifyInstance(testingData);
+        }
+    }
+    
+    @Override
+    public double[] distributionForInstance(Instance instance) throws NoSupportForMissingValuesException {
+        if (instance.hasMissingValue()) {
+          throw new NoSupportForMissingValuesException("MyID3 can't handle such missing value");
+        }
+        if (attrSeparator == null) {
+            return result;
+        } else { 
+            return child[(int) instance.value(attrSeparator)].distributionForInstance(instance);
+        }
+    }
+    
+    @Override
+    public Capabilities getCapabilities() {
+        Capabilities C = super.getCapabilities();
+        C.disableAll();
+
+        // attributes
+        C.enable(Capability.NOMINAL_ATTRIBUTES);
+
+        // class
+        C.enable(Capability.NOMINAL_CLASS);
+        C.enable(Capability.MISSING_CLASS_VALUES);
+
+        // instances
+        C.setMinimumNumberInstances(0);
+
+        return C;
+    }
+}
