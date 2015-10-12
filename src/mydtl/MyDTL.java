92c41e915fd03a44ee672f7ccde12f1ce90c39dc
andreysima <andrey.simaputera@yahoo.com>
2015-10-12 21:21:26 +0700

    done gan

--- src/mydtl/MyDTL.java
+++ src/mydtl/MyDTL.java
@@ -20,7 +20,7 @@ import weka.filters.unsupervised.attribute.Remove;
  */
 public class MyDTL {
     // Attribute
-    private static final String SOURCE = "data/iris.arff";
+    private static final String SOURCE = "data/weather.nominal.missing.arff";
     private static final int NUMBER_FOLD = 10;
     private static final int PERCENTAGE = 66;
     public static Instances data;
@@ -148,6 +148,6 @@ public class MyDTL {
         myID3 id3 = new myID3();
         myJ48 j48 = new myJ48();
         loadFile(SOURCE);
-        learn10fold(data, j48);
+        learn10fold(data, DT);
     }    
 }
